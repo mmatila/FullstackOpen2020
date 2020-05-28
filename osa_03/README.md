6f879bfe64d68f529d2f9f7c17cfefae87050cc6
mmatila <manu.matila1997@gmail.com>
2020-05-28 08:01:37 +0300

    Osa 3 done

---
+++ osa_03/README.md
@@ -0,0 +1,2 @@
+Heroku link:
+https://evening-lake-09289.herokuapp.com/
---
+++ osa_03/build/precache-manifest.91b967333a560fa29d667b16173d4e60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26b4b7bb21791e28e949a28fd97c7eb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "469315a3df0d6bd4509b",
+    "url": "/static/css/main.251313f5.chunk.css"
+  },
+  {
+    "revision": "27bd1cdff6215b46af31",
+    "url": "/static/js/2.fedff18b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fedff18b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "469315a3df0d6bd4509b",
+    "url": "/static/js/main.5ffee915.chunk.js"
+  },
+  {
+    "revision": "24aecb121d5a1e31ca85",
+    "url": "/static/js/runtime-main.455ce434.js"
+  }
+]);
