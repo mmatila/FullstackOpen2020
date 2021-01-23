7d02801fdcc6dd87b088860c74b3485f5bc0bb5d
Manu Matila <manu.matila1997@gmail.com>
2021-01-23 17:17:53 +0200

    7.6 done

--- osa_07/routed-anecdotes/src/App.js
+++ osa_07/routed-anecdotes/src/App.js
@@ -93,9 +93,9 @@ const Footer = () => (
 );
 
 const CreateNew = (props) => {
-  const content = useField("text");
-  const author = useField("text");
-  const info = useField("text");
+  const {reset: resetContent, ...content} = useField("text");
+  const {reset: resetAuthor, ...author} = useField("text");
+  const {reset: resetInfo, ...info} = useField("text");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -107,11 +107,11 @@ const CreateNew = (props) => {
     });
   };
 
-  const reset = (e) => {
+  const resetFields = (e) => {
     e.preventDefault();
-    content.reset();
-    author.reset();
-    info.reset();
+    resetContent();
+    resetAuthor();
+    resetInfo();
   };
 
   return (
@@ -131,7 +131,7 @@ const CreateNew = (props) => {
           <input {...info} />
         </div>
         <button>create</button>
-        <button onClick={reset}>reset</button>
+        <button onClick={resetFields}>reset</button>
       </form>
     </div>
   );
