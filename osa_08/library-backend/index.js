e9da13eb08b74f07e75615a614f876e64596318d
Manu Matila <manu.matila1997@gmail.com>
2021-08-27 07:51:14 +0300

    8.3 done

--- osa_08/library-backend/index.js
+++ osa_08/library-backend/index.js
@@ -88,6 +88,7 @@ const typeDefs = gql`
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 
   type Book {
@@ -97,14 +98,25 @@ const typeDefs = gql`
     id: ID!
     genres: [String!]!
   }
+
+  type Author {
+    name: String!
+    id: ID!
+    born: Int!
+    bookCount: Int!
+  }
 `;
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => authors
   },
+  Author: {
+    bookCount: ({ name }) => books.filter(({ author }) => author === name).length
+  }
 };
 
 const server = new ApolloServer({
