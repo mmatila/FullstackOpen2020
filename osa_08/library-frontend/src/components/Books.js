7a458c4ec69d1f800d9da1a9486343c56da1dc2b
Manu Matila <manu.matila1997@gmail.com>
2021-09-03 08:43:31 +0300

    8.19 done

--- osa_08/library-frontend/src/components/Books.js
+++ osa_08/library-frontend/src/components/Books.js
@@ -1,11 +1,21 @@
 import React, { useEffect, useState } from 'react'
+import _ from 'lodash';
 import { useQuery } from '@apollo/client';
 
 import { ALL_BOOKS } from '../queries';
 
 const Books = (props) => {
   const [books, setBooks] = useState([]);
+  const [filter, setFilter] = useState(null);
   const result = useQuery(ALL_BOOKS);
+
+  const genres = books.map(book => book.genres);
+  const flattened = _.flatten(genres);
+  const uniques = _.uniq(flattened)
+
+  const filteredBooks = filter
+    ? books.filter(book => book.genres.includes(filter))
+    : books
 
   useEffect(() => {
     if (result.data && result.data.allBooks !== null) setBooks(result.data.allBooks);
@@ -30,7 +40,7 @@ const Books = (props) => {
               published
             </th>
           </tr>
-          {books.map((b, index) =>
+          {filteredBooks.map((b, index) =>
             <tr key={index}>
               <td>{b.title}</td>
               <td>{b.author.name}</td>
@@ -39,6 +49,8 @@ const Books = (props) => {
           )}
         </tbody>
       </table>
+      {uniques.map(genre => <button key={genre} onClick={() => setFilter(genre)}>{genre}</button>)}
+      <button onClick={() => setFilter(null)}>all genres</button>
     </div>
   )
 }
--- osa_08/library-frontend/src/queries.js
+++ osa_08/library-frontend/src/queries.js
@@ -24,6 +24,7 @@ export const ALL_BOOKS = gql`
         born
         bookCount
       }
+      genres
     }
   }
 `;
