6d207ef180974f9d135dcf97a26a57a9cb809864
Manu Matila <manu.matila1997@gmail.com>
2021-09-04 17:44:12 +0300

    8.25 done

--- osa_08/library-frontend/src/App.js
+++ osa_08/library-frontend/src/App.js
@@ -5,16 +5,30 @@ import Books from './components/Books'
 import Login from './components/Login'
 import NewBook from './components/NewBook'
 import Recommended from './components/Recommended'
-import { BOOK_ADDED } from './queries'
+import { ALL_BOOKS, BOOK_ADDED } from './queries'
 
 const App = () => {
   const [page, setPage] = useState('authors');
   const [token, setToken] = useState(null);
   const client = useApolloClient();
 
+  const updateCacheWith = (addedBook) => {
+    const includedIn = (set, object) => 
+      set.map(p => p.id).includes(object.id)  
+
+    const dataInStore = client.readQuery({ query: ALL_BOOKS })
+    if (!includedIn(dataInStore.allBooks, addedBook)) {
+      client.writeQuery({
+        query: ALL_BOOKS,
+        data: { allBooks : dataInStore.allBooks.concat(addedBook) }
+      })
+    }   
+  }
+
   useSubscription(BOOK_ADDED, {
     onSubscriptionData: ({ subscriptionData }) => {
-      window.alert(subscriptionData);
+      const addedBook = subscriptionData.data.bookAdded
+      updateCacheWith(addedBook);
     }
   })
 
