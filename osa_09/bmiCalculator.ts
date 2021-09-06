d93815c8e1c8d502a885f0ec7e7fb6069754d4fa
Manu Matila <manu.matila1997@gmail.com>
2021-09-06 08:27:01 +0300

    9.5 done

--- osa_09/bmiCalculator.ts
+++ osa_09/bmiCalculator.ts
@@ -1,8 +1,4 @@
 type bmi = 'Underweight' | 'Normal (healthy weight)' | 'Overweight' | 'Obese';
-interface metrics {
-  height: number,
-  weight: number
-}
 
 const calculateBmi = (height: number, weight: number): bmi => {
   const bmi: number = weight / ((height / 100) ^ 2);
@@ -17,26 +13,5 @@ const calculateBmi = (height: number, weight: number): bmi => {
       return 'Obese';
   }
 };
-
-const parseArguments = (args: Array<string>): metrics => {
-  if (args.length < 4) throw new Error('Not enough arguments');
-  if (args.length > 4) throw new Error('Too many arguments');
-
-  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
-    return {
-      height: Number(args[2]),
-      weight: Number(args[3])
-    }
-  } else {
-    throw new Error('Provided values were not numbers!');
-  }
-}
-
-try {
-  const { height, weight } = parseArguments(process.argv);
-  console.log(calculateBmi(height, weight));
-} catch (error) {
-  console.log(error.message)
-}
 
 export default calculateBmi;
--- osa_09/index.ts
+++ osa_09/index.ts
@@ -1,9 +1,30 @@
 import express from 'express';
 
+import calculateBmi from './bmiCalculator';
+
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello!');
+});
+
+app.get('/bmi', (req, res) => {
+  const { height, weight } = req.query;
+  if (!height || !weight) {
+    res.status(400).json({ error: 'Query parameters "height" and "weight" required' });
+    return;
+  }
+  if (isNaN(Number(height)) || isNaN(Number(weight))) {
+    res.status(400).json({ error: 'Malformatted parameters' });
+    return;
+  }
+
+  const bmi = calculateBmi(Number(height), Number(weight)); 
+  res.json({
+    height: Number(height),
+    weight: Number(weight),
+    bmi,
+  });
 });
 
 const PORT = 3003
