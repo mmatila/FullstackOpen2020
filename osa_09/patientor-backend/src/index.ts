2c8286d6d5a571986b1793f3a2188e0a769378a6
Manu Matila <manu.matila1997@gmail.com>
2021-09-15 10:47:18 +0300

    9.10 done

--- osa_09/patientor-backend/src/index.ts
+++ osa_09/patientor-backend/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 
+import diagnoseRouter from './routes/diagnoses';
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -12,6 +14,9 @@ app.get('/ping', (_req, res) => {
   res.send('pong');
 });
 
+app.use('/api/diagnoses', diagnoseRouter);
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
