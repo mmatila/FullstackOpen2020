0c29afb10f9f85c48954c432af4e1e90cb3039dd
Manu Matila <manu.matila1997@gmail.com>
2021-09-21 09:33:27 +0300

    9.12 done

--- osa_09/patientor-backend/src/routes/patients.ts
+++ osa_09/patientor-backend/src/routes/patients.ts
@@ -8,4 +8,16 @@ router.get('/', (_req, res) => {
   res.send(patientsService.getPatients());
 });
 
+router.post('/', (req, res) => {
+  const { name, dateOfBirth, ssn, gender, occupation } = req.body;
+  const newPatient = patientsService.addPatient({
+    name,
+    dateOfBirth,
+    ssn,
+    gender,
+    occupation
+  });
+  res.json(newPatient);
+})
+
 export default router;
--- osa_09/patientor-backend/src/services/patientsService.ts
+++ osa_09/patientor-backend/src/services/patientsService.ts
@@ -1,11 +1,23 @@
 import { patients } from '../data/patients';
+import {v1 as uuid} from 'uuid';
 
-import { Patient } from '../types';
+import { NewPatient, Patient } from '../types';
 
 const getPatients = (): Omit<Patient, 'ssn' >[] => {
   return patients.map(({ ssn, ...rest }) => rest);
 };
 
+const addPatient = (patient: NewPatient): Patient => {
+  const newPatient = {
+    id: uuid(),
+    ...patient
+  }
+
+  patients.push(newPatient);
+  return newPatient;
+}
+
 export default {
-  getPatients
+  getPatients,
+  addPatient
 };
