c357b262f481543e21b429f7342488703d89ba90
Manu Matila <manu.matila1997@gmail.com>
2022-01-25 07:18:46 +0200

    9.17 done

--- osa_09/patientor-frontend/src/SinglePatientPage/index.tsx
+++ osa_09/patientor-frontend/src/SinglePatientPage/index.tsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import { apiBaseUrl } from '../constants';
+import { useStateValue } from '../state';
+import { Patient } from '../types';
+import { Divider } from 'semantic-ui-react';
 
 const SinglePatientPage = () => {
+  const [{ patient }, dispatch] = useStateValue();
   const { id } = useParams<{ id: string }>();
+
+  useEffect(() => {
+    const fetchPatient = async () => {
+      try {
+        const { data: patient } = await axios.get<Patient>(
+          `${apiBaseUrl}/patients/${id}`
+        );
+        dispatch({ type: "SET_PATIENT", payload: patient });
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    if (!patient || patient.id !== id) {
+      void fetchPatient();
+    }
+  }, [patient, id, dispatch]);
 
   return (
     <div>
-      <h1>Single Patient Page {id}</h1>
+      <h1>{patient?.name}</h1>
+      <Divider hidden />
+      <p>sex: {patient?.gender}</p>
+      <p>ssn: {patient?.ssn}</p>
+      <p>occupation: {patient?.occupation}</p>
     </div>
   );  
 };
